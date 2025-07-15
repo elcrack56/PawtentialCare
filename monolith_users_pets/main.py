56e863e2bfd769c3b9ff952065c7fac245e6024e
Dilan-Herrera <dilan.herrera@udla.edu.ec>
2025-07-15 04:51:47 -0500

    Add CORS middleware to enable frontend

--- monolith_users_pets/main.py
+++ monolith_users_pets/main.py
@@ -1,5 +1,20 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 import schemas
+
+app = FastAPI(title="Monolito: Usuarios y Mascotas")
+
+origins = [
+    "http://localhost:3000", 
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app = FastAPI()
 users_db = {}
